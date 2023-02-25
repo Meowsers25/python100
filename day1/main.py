527368ad06a7ed6b495066106823896cb6bbc719
Christian Lamothe <c.lamothe25@gmail.com>
2023-02-25 11:41:55 -0500

    Initial commit

---
+++ day1/main.py
@@ -0,0 +1,10 @@
+# print('Hello World')
+# print(len(input('What is your name?')))
+
+# name= input("What is your last name: ")
+# print(name)
+
+print('Welcome to the band name generator.')
+city = input('In what city did you grow up in?\n')
+pet = input('What is the name of your first pet?\n')
+print("Your band name could be " + city + " " + pet)
---
+++ day2/main.py
@@ -0,0 +1,45 @@
+# data types:
+# strings
+# integer
+# float = decimal nums
+# booloean True False
+# len() doesnt work with integers
+
+# print("hello"[-1])
+
+# num_char = len(input("What is your name? \n"))
+# type conversion
+# print("Your name has " + str(num_char) + " characters.")
+
+# data types challenge
+# new_num = input('Please enter a 2 digit number:\n')
+# num_1 = int(new_num[0])
+# num_2 = int(new_num[1])
+# print(num_1 + num_2)
+
+# math operations
+# +, -, *, /, **(exponent)
+# / always returns a float
+# PEMDAS
+
+# BMI calculator
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+
+# bmi = int(weight) / float(height) ** 2
+# bmi_as_int = int(bmi )
+# print(bmi_as_int)
+
+# use a comma to tell how many digits after decimal
+print(round(8 / 3, 2))
+
+# floor division use 2 //
+print(8 // 3)
+
+# can use += tp increment like JS
+
+# f-strings are fucking awesome
+
+score = 95
+age = 51
+print(f"Your score is {score} and you're {age} years old.")
